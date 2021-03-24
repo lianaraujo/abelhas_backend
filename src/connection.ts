75be4e693f3891674b277c845fb4e2c4c5b19557
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 20:56:07 -0300

    env check

--- src/connection.ts
+++ src/connection.ts
@@ -1,6 +1,11 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+  throw new Error('no URI');
+}
+
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
