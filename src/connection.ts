ab538615c3781a3edc1b3c0e6331b95648e7baea
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 16:05:16 -0300

    connection fix

--- src/connection.ts
+++ src/connection.ts
@@ -1,8 +1,10 @@
 import mongoose from 'mongoose';
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
+import Config from './utils/config-service';
+
+mongoose.connect(Config.getString('MONGODB_URI'), { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-console.log("connected");
+db.once('open', function () {
+  console.log("connected");
 });
