02c2247f2a0e241d7189417cc5e102ea410de177
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 18:42:08 -0300

    ????

--- src/server.ts
+++ src/server.ts
@@ -7,21 +7,18 @@ import "./connection";
 import cors from "cors";
 import routes from "./routes";
 import errorHandler from "./errors/handler";
-import Config from "./utils/config-service";
 
 const app = express();
 
 app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
+  cors()
 );
 app.use(express.json());
 app.use(routes);
 
 app.use(errorHandler);
 
-app.listen(Config.getString('PORT') || 3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("🍯 Listening on port 3333 🍯");
 });
 
