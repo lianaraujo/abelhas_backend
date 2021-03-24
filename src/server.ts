6e2a91148ac738ceda30ec33257063b0348ab391
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 19:22:48 -0300

    test

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import "express-async-errors";
 
-import 'dotenv/config';
 import "./env/config";
+
+import 'dotenv/config';
+
 import "./connection";
 
 import cors from "cors";
