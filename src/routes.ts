4d94e3ee8106db1fad3325e3803087c1dd4c5592
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 15:58:11 -0300

    clean up

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,13 @@
 import { Router } from "express";
-import AuthController from "./controllers/AuthController";
 import TotemController from "./controllers/TotemController";
-import UserController from "./controllers/UserController";
 
 const routes = Router();
-
-routes.get("/users/:id", UserController.show);
-routes.post("/users", UserController.create); ///#################
-
-routes.post("/auth/login", AuthController.login); //##################
-routes.post("/auth/forgot", AuthController.forgotPassword);
-routes.post("/auth/recover", AuthController.recoverPassword);
+;
 
 routes.get("/totems/:id", TotemController.show);
 routes.get("/totems", TotemController.index);
 routes.post("/totems", TotemController.create); //// ######
 routes.delete("/totems/:id", TotemController.delete)
 
-routes.use(AuthController.autorization); //// #############
 
 export default routes;
