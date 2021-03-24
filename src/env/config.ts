662d57eaf31d28b9ba112d08c6c89285b36c64b5
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 19:46:53 -0300

    .env

--- src/env/config.ts
+++ src/env/config.ts
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-const envFile = path.resolve(__dirname, '.env');
+const envFile = path.resolve('.env');
 
 const result = dotenv.config({ path: envFile });
 if (result.error) {
