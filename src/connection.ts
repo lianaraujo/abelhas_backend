539e09099e77f39486630fedc8a7c7f22ef1fb0a
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-24 15:12:11 -0300

    heroku setup

--- src/connection.ts
+++ src/connection.ts
@@ -1,7 +1,5 @@
 import mongoose from 'mongoose';
-mongoose.connect('mongodb+srv://pepega:jesusmorreunacruzedisseai@cluster0.eeack.mongodb.net/dbian?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
-
-// mongoose.connect('mongodb+srv://dbaian:H7wrgJ9jXilDF3u8@cluster0.oouhp.mongodb.net/ian?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ app.use(
 app.use(express.json());
 app.use(routes);
 
+
 app.listen(process.env.PORT || 3333, () => {
   console.log("🍯 Listening on port 3333 🍯");
 });
