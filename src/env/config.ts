9ac7c329b5a478d2030b2393b0d6cee7e352e17a
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-29 01:00:11 -0300

    env config

--- src/env/config.ts
+++ src/env/config.ts
@@ -1,9 +1,14 @@
 import dotenv from 'dotenv';
 import path from 'path';
 
-const envFile = path.resolve(__dirname, '.env');
+try {
+  const envFile = path.resolve(__dirname, '.env');
 
-const result = dotenv.config({ path: envFile });
-if (result.error) {
-  throw result.error;
+  const result = dotenv.config({ path: envFile });
+  if (result.error) {
+    throw result.error;
+  }
+} catch (error) {
+  console.log('.env not found')
 }
+
