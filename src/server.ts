f40f5163df4abb14556735d0a53e6a59af1cb06b
Lian Araújo Fernandes <liansutil@gmail.com>
2021-03-29 00:42:49 -0300

    deploy

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import "express-async-errors";
 
-import "../env/config";
+import "./env/config";
 import "./connection";
 
 import cors from "cors";
@@ -12,13 +12,12 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
   })
 );
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
   console.log("🍯 Listening on port 3333 🍯");
 });
 
