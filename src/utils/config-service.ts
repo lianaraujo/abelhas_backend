63913df74b489be7ddd5f3f9f6c0505a37d57b1b
Lian Araújo Fernandes <liansutil@gmail.com>
2021-04-01 21:51:24 -0300

    deleted console.logs from config-service.ts

--- src/utils/config-service.ts
+++ src/utils/config-service.ts
@@ -1,8 +1,6 @@
 class Config {
   static getString(name: string): string {
-    console.log(name)
     const response = process.env[name];
-    console.log(process.env)
     if (!response) {
       throw new Error('[Config] data not found');
     }
